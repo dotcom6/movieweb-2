2ba0a2a8f7de0b632b75ea1833e1c37730b790e6
dotcom6 <146667870+dotcom6@users.noreply.github.com>
2024-03-25 12:43:28 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
+  VITE_CORS_PROXY_URL: "https://movies-r-fun.pancakesarepowerful.workers.dev/",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDNjMTMyZDY5NDdjMzBmZTlkYzU3YmQzOTYwZmMwMiIsInN1YiI6IjY1ZDhlYWQxYjA0NjA1MDE4NzM0NjU0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BJpker9n4WN1tnF59WLh_wUplGy11oomrOc1THBSpUs",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: false,
+
+  // The backend URL to communicate with
+  VITE_BACKEND_URL: null,
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: "",
+};
